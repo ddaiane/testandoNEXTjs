693e4a57756a44a99e77f150b94d7d974ef6a6c3
Daiane Marcon <daianemarcon@gmail.com>
2023-01-13 16:38:02 -0300

    muda texto pagina voos

--- src/screens/voosScreen.js
+++ src/screens/voosScreen.js
@@ -4,22 +4,14 @@ import Title from "../components/Title";
 
 export default function faqScreen({voos}) {
   const voosOrdenado = voos.departures ? voos.departures.reverse() : voos; //saida mais próxima primeiro;
+  const arrivalHour = new Date(voos.departures[0].arrival.scheduledTimeLocal);
   
     return (
       <>
-      <PageTitle>VOOS</PageTitle>
-      <Title as="h2">Não aguenta mais Porto Alegre?</Title>
-      <h3>Então pega ai o próximo voo pra ir embora daqui:</h3>
-      <div>
-        {voos.departures.map((voo) => {
-            const arrivalHour = new Date(voo.arrival.scheduledTimeLocal);
-            return (
-                <>
-                <Title as="h3">{`O voo ${voo.number} da ${voo.airline.name} chega as ${arrivalHour.getHours()}h e ${arrivalHour.getMinutes()} minutos em ${voo.arrival.airport.name}`}</Title>
-                </>
-            )
-        })}
-      </div>
+      <PageTitle>CHEGA</PageTitle>
+      <Title as="h2">Também não aguenta mais Porto Alegre?</Title>
+      <h3>O próximo voo saindo de Porto Alegre pra ir embora daqui é</h3>
+        <Title as="h3">{`o voo ${voos.departures[0].number} da ${voos.departures[0].airline.name} que chega as ${arrivalHour.getHours()}h e ${arrivalHour.getMinutes()} minutos em ${voos.departures[0].arrival.airport.name}`}</Title>
       <Link href="/">
         Ir para home
       </Link>
